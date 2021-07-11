d28f5a91d5a947a3495762abab8dfd68dfcc4bc9
JohnMHoyt24 <67488201+JohnMHoyt24@users.noreply.github.com>
2021-07-11 14:34:10 -0400

    first commit

---
+++ API/WebAPI/Web API/Web API/Controllers/DepartmentController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Web_API.Models;
+
+namespace Web_API.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            //Find out how I can create stored procedures for these SQL queries.
+            string query = @"
+                    select DepartmentId,DepartmentName from
+                    dbo.Department
+                    ";
+            
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+            ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using(var cmd= new SqlCommand(query,con))
+            using(var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+        }
+
+        public string Post(Department dep)
+        {
+            try
+            {
+                string query = @"
+                    insert into dbo.Department values
+                    ('"+dep.DepartmentName+ @"')
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Added Successfully!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Add!";
+            }
+        }
+
+        public string Put(Department dep)
+        {
+            try
+            {
+                string query = @"
+                    update dbo.Department set DepartmentName=
+                    '" + dep.DepartmentName + @"'
+                    where DepartmentId=" + dep.DepartmentId + @"
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Updated Successfully!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Update!";
+            }
+        }
+
+        public string Delete(Department dep)
+        {
+            try
+            {
+                string query = @"
+                    delete from dbo.Department 
+                    where DepartmentId=" + dep.DepartmentId + @"
+                    ";
+
+                DataTable table = new DataTable();
+                using (var con = new SqlConnection(ConfigurationManager.
+                ConnectionStrings["EmployeeAppDB"].ConnectionString))
+                using (var cmd = new SqlCommand(query, con))
+                using (var da = new SqlDataAdapter(cmd))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    da.Fill(table);
+                }
+
+                return "Deleted Successfully!";
+            }
+            catch (Exception)
+            {
+
+                return "Failed to Delete!";
+            }
+        }
+    }
+}
---
+++ API/WebAPI/Web API/Web API/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Web_API.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            //Find out how I can create stored procedures for these SQL queries.
+            string query = @"
+                    select EmployeeId,EmployeeName,Department,DateOfJoining,PhotoFileName from
+                    dbo.Employee
+                    ";
+
+            DataTable table = new DataTable();
+            using (var con = new SqlConnection(ConfigurationManager.
+            ConnectionStrings["EmployeeAppDB"].ConnectionString))
+            using (var cmd = new SqlCommand(query, con))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.CommandType = CommandType.Text;
+                da.Fill(table);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, table);
+
+        }
+    }
+}
